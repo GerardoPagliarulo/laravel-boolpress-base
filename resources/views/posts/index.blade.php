6ffce657a99460b0035c20b65ae04c59486a7c88
Gerardo Pagliarulo <gerardopagliarulo3@gmail.com>
2020-06-22 22:28:22 +0200

    Laravel - Boolpress - Base

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,10 +4,10 @@
         <h1>Posts Archive</h1>
         @foreach ($posts as $post)
             <article>
-                <h2>{{$post->title}}</h2>
-                <h4>Date of create: {{$post->created_at}} - Last edit: {{$post->updated_at}}</h4>
-                <p>{{$post->body}}</p>
                 <h4>{{$post->user->name}}</h4>
+                <h4>Date of create: {{$post->created_at}} - Last edit: {{$post->updated_at}}</h4>
+                <h2>{{$post->title}}</h2>
+                <p>{{$post->body}}</p>
             </article>
             @if (!$loop->last)
                 <hr>
